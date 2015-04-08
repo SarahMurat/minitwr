3866d4a082570b553a4248ec6f7b6885972c500e
elisakalbe <elisa.kalbe@etu.unistra.fr>
2015-04-08 15:02:49 +0200

    Ajout et affichage des tweets envoyés

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var tweets=[];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', tweets: tweets });
+});
+
+/* Creer formulaire et poster infos sur url : contenu tweet */
+router.post('/', function(req, res, next) {
+   tweets.unshift(req.body.tweet); /* unshift : ajoute dans var tweet (~push) req.body.tweet : contenu */
+    res.redirect('/') /* redirige sur la page d'accueil */
 });
 
 module.exports = router;
+
