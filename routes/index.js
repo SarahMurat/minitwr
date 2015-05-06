95139f20722180a3404131296576ddea229ef71b
elisakalbe <elisa.kalbe@etu.unistra.fr>
2015-05-06 14:53:05 +0200

    Modification design + base de la page de profil

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,11 @@ router.get('/accueil', function(req, res, next) {
   res.render('accueil', { title: 'Express', tweets: tweets });
 });
 
+router.get('/profil', function(req, res, next) {
+  res.render('profil', { title: 'Express', tweets: tweets });
+});
 
-/* Creer formulaire et poster infos sur url : contenu tweet */
+/* Creer formulaire et poster infos sur url : contenu tweet - sauvegarde les tweets. */
 router.post('/accueil', function(req, res, next) {
    tweets.unshift(req.body.tweet); /* unshift : ajoute dans var tweet (~push) req.body.tweet : contenu */
     res.redirect('/accueil') /* redirige sur la page d'accueil */
